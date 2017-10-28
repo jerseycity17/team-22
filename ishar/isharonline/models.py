8995ec7364651fdafee0e78b528fcb5e149f5bcf
ethklein <ethanklein@gwu.edu>
2017-10-27 20:25:06 -0400

    django app

---
+++ ishar/isharonline/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+# Leverage Django's built-in User models
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Ishar(models.Model):
+    user =  models.ForeignKey(User, null=True, blank=True)
+    description = models.CharField(max_length=128, null=False, blank=False)
+    due_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        """this sets the default return for this object"""
+        return self.description
