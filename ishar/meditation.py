24b4e2fba5b15810034fc35e0ef7df254fac567d
Yong-L <dkrcjs7521@gmail.com>
2017-10-28 01:53:36 -0400

    Meditation JSON dump complete

--- ishar/meditation.py
+++ ishar/meditation.py
@@ -4,6 +4,30 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ishar.settings")
 django.setup()
 
 from isharonline.models import meditations
+import json
 
-all_entries = meditations.objects.all()
-print(all_entries)
+top10 = meditations.objects.all().order_by('-PublicationYear')[:10]
+
+cache = {}
+
+category = ['Key','ItemType','PublicationYear','Author','Title','PublicationTitle','ISBN','ISSN','DOI','Url','AbstractNote','Date',
+            'DateAdded','DateModified','AccessDate','Pages','NumPages','Issue','Volume','NumberOfVolumes','JournalAbbreviation',
+            'ShortTitle','Series','SeriesNumber','SeriesText','SeriesTitle','Publisher','Place','Language','Rights','Type',
+            'Archive','ArchiveLocation','LibraryCatalog','CallNumber','Extra','Notes','FileAttachments','LinkAttachments',
+            'ManualTags','AutomaticTags','Editor','SeriesEditor','Translator','Contributor','AttorneyAgent','BookAuthor',
+            'CastMember','Commenter','Composer','Cosponsor','Counsel','Interviewer','Producer','Recipient','ReviewedAuthor',
+            'Scriptwriter','WordsBy','Guest','Number','Edition','RunningTime','Scale','Medium','ArtworkSize','FilingDate',
+            'ApplicationNumber','Assignee','IssuingAuthority','Country','MeetingName','ConferenceName','Court','References',
+            'Reporter','LegalStatus','PriorityNumbers','ProgrammingLanguage','Version','System','Code','CodeNumber','Section',
+            'Session','Committee','History','LegislativeBody']
+
+for i, v in enumerate(top10):
+    fields = str(v).split("|")
+    print(fields)
+    cache_tmp = {}
+    for j, f in enumerate(fields):
+        cache_tmp[category[j]] = f
+    cache[i + 1] = cache_tmp
+
+with open('top10.json', 'w') as outfile:
+    json.dump(cache, outfile)
