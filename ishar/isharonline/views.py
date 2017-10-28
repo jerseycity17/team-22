0baeceb98ba4d181783a7d3796678bae2303ff0c
Gary <gary.coltrane1@marist.edu>
2017-10-28 00:53:55 -0400

    working towards lib page

--- ishar/isharonline/views.py
+++ ishar/isharonline/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "isharonline/index.html")
+
+
+def library(request):
+    return render(request, "isharonline/library.html")
