029e58530d35745a3eb9ac342f42c7f61283189c
Yong-L <dkrcjs7521@gmail.com>
2017-10-28 02:27:36 -0400

    meditations commit

--- ishar/meditation.py
+++ ishar/meditation.py
@@ -6,28 +6,28 @@ django.setup()
 from isharonline.models import meditations
 import json
 
-top10 = meditations.objects.all().order_by('-PublicationYear')[:10]
+def landingtop10():
+    top10 = meditations.objects.all().order_by('-PublicationYear')[:10]
 
-cache = {}
+    cache = {}
 
-category = ['Key','ItemType','PublicationYear','Author','Title','PublicationTitle','ISBN','ISSN','DOI','Url','AbstractNote','Date',
-            'DateAdded','DateModified','AccessDate','Pages','NumPages','Issue','Volume','NumberOfVolumes','JournalAbbreviation',
-            'ShortTitle','Series','SeriesNumber','SeriesText','SeriesTitle','Publisher','Place','Language','Rights','Type',
-            'Archive','ArchiveLocation','LibraryCatalog','CallNumber','Extra','Notes','FileAttachments','LinkAttachments',
-            'ManualTags','AutomaticTags','Editor','SeriesEditor','Translator','Contributor','AttorneyAgent','BookAuthor',
-            'CastMember','Commenter','Composer','Cosponsor','Counsel','Interviewer','Producer','Recipient','ReviewedAuthor',
-            'Scriptwriter','WordsBy','Guest','Number','Edition','RunningTime','Scale','Medium','ArtworkSize','FilingDate',
-            'ApplicationNumber','Assignee','IssuingAuthority','Country','MeetingName','ConferenceName','Court','References',
-            'Reporter','LegalStatus','PriorityNumbers','ProgrammingLanguage','Version','System','Code','CodeNumber','Section',
-            'Session','Committee','History','LegislativeBody']
+    category = ['Key','ItemType','PublicationYear','Author','Title','PublicationTitle','ISBN','ISSN','DOI','Url','AbstractNote','Date',
+                'DateAdded','DateModified','AccessDate','Pages','NumPages','Issue','Volume','NumberOfVolumes','JournalAbbreviation',
+                'ShortTitle','Series','SeriesNumber','SeriesText','SeriesTitle','Publisher','Place','Language','Rights','Type',
+                'Archive','ArchiveLocation','LibraryCatalog','CallNumber','Extra','Notes','FileAttachments','LinkAttachments',
+                'ManualTags','AutomaticTags','Editor','SeriesEditor','Translator','Contributor','AttorneyAgent','BookAuthor',
+                'CastMember','Commenter','Composer','Cosponsor','Counsel','Interviewer','Producer','Recipient','ReviewedAuthor',
+                'Scriptwriter','WordsBy','Guest','Number','Edition','RunningTime','Scale','Medium','ArtworkSize','FilingDate',
+                'ApplicationNumber','Assignee','IssuingAuthority','Country','MeetingName','ConferenceName','Court','References',
+                'Reporter','LegalStatus','PriorityNumbers','ProgrammingLanguage','Version','System','Code','CodeNumber','Section',
+                'Session','Committee','History','LegislativeBody']
 
-for i, v in enumerate(top10):
-    fields = str(v).split("|")
-    print(fields)
-    cache_tmp = {}
-    for j, f in enumerate(fields):
-        cache_tmp[category[j]] = f
-    cache[i + 1] = cache_tmp
+    for i, v in enumerate(top10):
+        fields = str(v).split("|")
+        print(fields)
+        cache_tmp = {}
+        for j, f in enumerate(fields):
+            cache_tmp[category[j]] = f
+        cache[i + 1] = cache_tmp
 
-with open('top10.json', 'w') as outfile:
-    json.dump(cache, outfile)
+    return json.dumps(cache)
